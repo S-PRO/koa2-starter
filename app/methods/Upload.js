425a6a15d4db17d32cd064e0b9c9d2a4a1f399c7
bogdantarasenk <btarasenko@spro.io>
2017-03-28 18:10:04 +0300

    fix linter

--- app/methods/Upload.js
+++ app/methods/Upload.js
@@ -18,12 +18,12 @@ export default class Upload {
     console.log(path);
     await files.map((file) => {
       fs.readFile(file.path, (err, data) => {
-        let filename = uuid.v4();
-        let localpath = '/upload/';
-        let newPath = `${__dirname}${localpath}${filename}.png`;
+        const filename = uuid.v4();
+        const localpath = '/upload/';
+        const newPath = `${__dirname}${localpath}${filename}.png`;
         console.log('here');
         console.log(newPath);
-        fs.writeFile(newPath, data, function(error) {
+        fs.writeFile(newPath, data, (error) => {
           console.log(error);
         });
       });
